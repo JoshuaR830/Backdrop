f497d8cafb3415406273989e01d32a6bf1936e85
Joshua <psyjr4@nottingham.ac.uk>
2020-01-11 00:25:11 +0000

    Splitting out the backdrop layout of another repo for easy reuse

---
+++ src/public/js/backdrop.js
@@ -0,0 +1,61 @@
+window.addEventListener('load', function() {
+    var buttons = document.querySelectorAll('.button');
+    var foreground = document.getElementById('foreground');
+    var menuReveal = document.getElementById("menu-reveal");
+    var menuHide = document.getElementById('menu-hide');
+    var container = document.querySelector('.content-container');
+    var reveal = document.querySelector('.subtitle-container');
+    var foregroundButton = document.querySelector('.show-foreground-button');
+
+    menuReveal.addEventListener('click', function(event) {
+        console.log("menu clicked");
+        menuRevealed();
+        foreground.classList.add('animated-scroll-forwards');
+        foreground.classList.remove('animated-scroll-backwards');
+        foregroundButton.style.display = 'inline-block';
+        reveal.classList.add('reveal-foreground');
+    });
+
+    var menuHide = document.getElementById('menu-hide');
+    menuHide.addEventListener('click', function(event) {
+        displayForeground();
+    });
+
+    buttons.forEach(element => {
+        console.log(element);
+        element.addEventListener('mouseenter', function() {
+            element.querySelector('.image-container').classList.add('button-enter');
+            element.querySelector('.image-container').classList.remove('button-leave');
+        });
+
+        element.addEventListener('mouseleave', function() {
+            element.querySelector('.image-container').classList.remove('button-enter');
+            element.querySelector('.image-container').classList.add('button-leave');
+        });
+    });
+
+    reveal.addEventListener('click', function() {
+        if(reveal.classList.contains('reveal-foreground')){
+            displayForeground();
+        }
+    });
+
+    function displayForeground() {
+        foregroundButton.style.display = 'none';
+        reveal.classList.remove('reveal-foreground');
+        foreground.classList.add('animated-scroll-backwards');
+        foreground.classList.remove('animated-scroll-forwards');
+        container.style.display = 'inline-block';
+        menuHidden();
+    }
+
+    function menuRevealed() {
+        menuHide.style.display = 'inline-block';
+        menuReveal.style.display = 'none';
+    }
+
+    function menuHidden() {
+        menuHide.style.display = 'none';
+        menuReveal.style.display = 'inline-block';
+    }
+});
